a463416261842bd63e9b7b2a06e0eeb8bfcd9a2b
Delon Newman <contact@delonnewman.name>
2022-02-16 22:59:52 -0700

    Load gui config when started in daemon mode

--- elisp/drn-appearance.el
+++ elisp/drn-appearance.el
@@ -4,22 +4,25 @@
 ;(require 'color-theme-buffer-local)
 
 ;; font
-(set-face-attribute 'default nil :font "Fira Code-15")
-(set-frame-font "Fira Code-15" nil t)
+(setq default-frame-alist '((font . "Fira Code-15")))
 
 ;; add theme directory to load path
 (add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
 
-(defvar drn-theme 'nord)
+;(setq custom-enabled-themes '(nord))
 
-(load-theme drn-theme t)
-(set-frame-parameter nil 'background-mode 'dark)
-(enable-theme drn-theme)
+                                        ;(defvar drn-theme 'nord)
+(if (daemonp)
+    (add-hook 'after-make-frame-functions
+        (lambda (frame)
+            (with-selected-frame frame
+              (set-frame-size (selected-frame) 120 80)
+              (load-theme 'nord t))))
+    (load-theme 'nord t))
 
 (column-number-mode t)
 (tool-bar-mode -1)
 
-(when window-system (set-frame-size (selected-frame) 120 80))
 
 ;; make titlebar transparent on macOS
 (when (eq system-type 'darwin) (ns-auto-titlebar-mode))
